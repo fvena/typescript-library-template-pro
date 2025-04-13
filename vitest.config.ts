3392af64042d04c9570deb613b0a5d5c96913271
Francisco Vena <fvena32@gmail.com>
2025-04-13 13:30:01 +0200

    chore: update commitlint configuration and bump various dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import swc from "unplugin-swc";
 
 export default defineConfig({
   plugins: [
-     
     swc.vite({
       module: { type: "es6" },
     }),
@@ -11,6 +10,7 @@ export default defineConfig({
   test: {
     clearMocks: true,
     coverage: {
+      exclude: ["**/*.d.ts"],
       include: ["src/**/*.ts"],
       provider: "v8",
       reporter: ["text", "lcovonly"],
