3243e80a99ca54ba6f819ce60dca2c99d60ffa63
Francisco Vena <fvena32@gmail.com>
2025-04-17 14:09:32 +0200

    chore: update tsup configuration for improved build process and adjust playground paths

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,3 @@
-import type { Options } from "tsup";
 import { defineConfig } from "tsup";
 
 /**
@@ -8,40 +7,17 @@ import { defineConfig } from "tsup";
  * and bundlers. It produces both formats from a single configuration which
  * simplifies maintenance and ensures consistency across outputs.
  */
-const nodeConfig: Options = {
-  clean: true, // Remove previous build files before each build
-  dts: true, // Generate TypeScript declaration files (.d.ts)
-  entry: ["src/index.ts"], // The entry points to your library. You can add more entry points if needed.
-  format: ["esm", "cjs"], // Generates both ESM and CJS formats simultaneously
-  minify: true, // Minifies the output to reduce bundle size
-  outDir: "./dist", // Directory where build files will be output
-  sourcemap: true, // Generate source maps to make debugging easier
-  treeshake: true, // Enables tree shaking to remove unused code and reduce bundle size
-};
-
-/**
- * tsup Configuration Function
- *
- * This function returns different configurations based on the build context.
- * It's especially useful for providing optimized development and production builds.
- */
 export default defineConfig((options) => {
-  // Development mode (watch)
-  // When running with --watch flag, we optimize for faster rebuilds
-  // by disabling minification and only building the Node.js formats
-  if (options.watch) {
-    return [
-      {
-        ...nodeConfig,
-        minify: false, // This is enabled by default but disabled in watch mode for faster development
-        ...options,
-      },
-    ];
-  }
+  const isWatch = !!options.watch;
 
-  // Production mode
-  // Build all formats with optimizations enabled
-  return [
-    { ...nodeConfig, ...options }, // Node.js builds (ESM, CJS)
-  ];
+  return {
+    clean: true, // Remove previous build files before each build
+    dts: true, // Generate TypeScript declaration files (.d.ts)
+    entry: ["src/index.ts"], // The entry points to your library. You can add more entry points if needed.
+    format: ["esm", "cjs"], // Generates both ESM and CJS formats simultaneously
+    minify: !isWatch, // Minifies the output to reduce bundle size
+    outDir: "./dist", // Directory where build files will be output
+    sourcemap: true, // Generate source maps to make debugging easier
+    treeshake: true, // Enables tree shaking to remove unused code and reduce bundle size
+  };
 });
